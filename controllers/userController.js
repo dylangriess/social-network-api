671318fdd6062bff6abc5c184c2c2ad69d43968c
dylangriess <dylangriess@gmail.com>
2022-09-28 18:40:20 -0600

    added user Routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,67 @@
 // --> /api/users
+const { json } = require("express");
+const { User, Thought } = require("../models");
 
-//GET all users
+module.exports = {
+  //GET all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  //GET a single user by its _id and populated thought and friend data
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "No user with that ID!",
+            })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  //POST a new user:
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  //PUT to update a user by its _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.UserId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User with this ID!" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  //DELETE to remove user by its _id
+  //BONUS: Remove a user's associated thoughts when deleted.
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() =>
+        res.json({ message: "User and associated thoughts deleted!" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
-//GET a single user by its _id and populated thought and friend data
+  // --> /api/users/:userId/friends/:friendId
 
-//POST a new user:
+  //POST to add a new friend to a user's friend list
 
-//PUT to update a user by its _id
-
-//DELETE to remove user by its _id
-
-//BONUS: Remove a user's associated thoughts when deleted.
-
-// --> /api/users/:userId/friends/:friendId
-
-//POST to add a new friend to a user's friend list
-
-//DELETE to remove a friend from a user's friend list
+  //DELETE to remove a friend from a user's friend list
+};
